31466ec3d9e4b19492c10b836cb07b9bdc09fae3
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-01-04 19:51:20 +0100

    change username to variable

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,29 +9,30 @@ apt_repository "sabnzbd" do
 end
 
 %w{
-    sabnzbdplus sabnzbdplus-theme-smpl
-    sabnzbdplus-theme-plush sabnzbdplus-theme-iphone
-    }.each do |pkg|
-        package pkg do
-            action :upgrade
-        end
-    end
+  sabnzbdplus sabnzbdplus-theme-smpl
+  sabnzbdplus-theme-plush sabnzbdplus-theme-iphone
+  }.each do |pkg|
+      package pkg do
+          action :upgrade
+      end
+  end
 
 template "/etc/default/sabnzbdplus" do
-    source "sabnzbd.erb"
-    mode 0644
-    notifies :restart, 'service[sabnzbdplus]'
+  source "sabnzbd.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :restart, 'service[sabnzbdplus]'
 end
 
-template "/home/thomas/.sabnzbd/sabnzbd.ini" do
-    source "sabnzbd.ini.erb"
-    mode 0600
-    notifies :restart, 'service[sabnzbdplus]'
-    owner node["sabnzbd"]["user"]
-    group node["sabnzbd"]["group"]
+template "/home/#{node['sabnzbd']['user']}/.sabnzbd/sabnzbd.ini" do
+  source "sabnzbd.ini.erb"
+  mode 0600
+  notifies :restart, 'service[sabnzbdplus]'
+  owner node["sabnzbd"]["user"]
+  group node["sabnzbd"]["group"]
 end
 
-# typically this will run /etc/init.d/example_service start
 service "sabnzbdplus" do
   action :start
 end
